d5888381eef74ebb7e20c38234b47de03d15fd0e
Mia Moretti <mimickal.dev@gmail.com>
2018-01-21 22:56:12 -0500

    Bump version

--- src/main/java/mimickal/mc/mcdrops/DropsMod.java
+++ src/main/java/mimickal/mc/mcdrops/DropsMod.java
@@ -18,9 +18,10 @@ import org.apache.logging.log4j.Logger;
 )
 public class DropsMod {
 
+    // NOTE info duplicated in build.gradle
     public static final String MODID = "mcdrops";
     public static final String NAME = "MCDrops";
-    public static final String VERSION = "1.10.2-1.0.0";
+    public static final String VERSION = "1.10.2-1.1.0";
     public static final Logger LOGGER = LogManager.getLogger(MODID);
 
     @EventHandler
